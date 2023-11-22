a32bc744bd1c2fb45df9420ddcb6d5e54c819044
ZekkenCL <benito.munoz@alumnos.ucn.cl>
2023-11-22 14:40:32 -0300

    feat: backend

---
+++ backend/app/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+migrate = Migrate()
+db = SQLAlchemy()
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:beno1989@host/nombre_base_datos'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    return app
